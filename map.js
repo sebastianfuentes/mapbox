75e3cc3a88902efd7f37f56da80340beb8ec5f00
sebastianfuentes <sebas6297@gmail.com>
2015-03-19 15:02:49 -0600

    commit adding the route tracer

--- map.js
+++ map.js
@@ -9,32 +9,6 @@ var map = L.mapbox.map('map', 'yalku.lekd3g9a', {
     }}).setView([22,-101],6);//coordinates for the view
 map.legendControl.addLegend('<strong>My first map for yalku</strong>'); //content of the layer
 
-var polyline = L.polyline([]).addTo(map);
-
-// Keep a counter of how many points we've added to the map.
-var pointsAdded = 0;
-
-// Start drawing the polyline.
-
-function add() {
-
-    // `addLatLng` takes a new latLng coordinate and puts it at the end of the
-    // line. You optionally pull points from your data or generate them. Here
-    // we make a sine wave with some math.
-    polyline.addLatLng(
-        L.latLng(
-            Math.cos(pointsAdded / 40) * 30,
-            pointsAdded));
-
-    // Pan the map along with where the line is being added.
-    map.setView([0, pointsAdded], 3);
-
-    // Continue to draw and pan the map by calling `add()`
-    // until `pointsAdded` reaches 360.
-    if (++pointsAdded < 400) window.setTimeout(add, 100);
-    console.log(pointsAdded)
-}
-
 map.on('dblclick', function setMarker(coordinates) {//function to obtain the coordinates with double click on the map
     var latitude = coordinates.latlng.lat;
     var longitude = coordinates.latlng.lng;
@@ -69,9 +43,9 @@ markerLayer.setGeoJSON(marker);//we add the marker to the map
 console.log("si imprime el marcador");
 return marker;
 });
-map.featureLayer.on('dblclick', function(e) {
+map.featureLayer.on('ready', function(e) {
   var line = [];
-
+  console.log("si entra a la funcion")
   this.eachLayer(function(marker) {
     alert(marker.getLatLng());
     line.push(marker.getLatLng());
