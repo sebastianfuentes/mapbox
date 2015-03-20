b65ee599705e182f3343bd2de384b1d6a894d617
sebastianfuentes <sebas6297@gmail.com>
2015-03-20 13:18:17 -0600

    :bowtie:traversing the input to obtain the coordinates in the place

--- map.js
+++ map.js
@@ -14,47 +14,35 @@ map.on('dblclick', function setMarker(coordinates) {//function to obtain the coo
     var longitude = coordinates.latlng.lng;
     var position = [longitude,latitude];
 // all coordinates and info of the first marker in here
-var marker = [
-                {
-                "type": "FeatureCollection",
-                "features": [
-                  {
-                    "type": "Feature",
-                    "properties": {},
-                    "geometry": {
-                      "type": "Point",
-                      "coordinates": [
-                        longitude,
-                        latitude
-                      ]
-                    },
-                    "properties": {
-                      "title": "The Pool ",
-                      "marker-color": "#3BCF56",
-                      "marker-size": "medium",
-                      "marker-symbol": "rocket"
-                    }
+    var marker = [
+                    {
+                    "type": "FeatureCollection",
+                    "features": [
+                      {
+                        "type": "Feature",
+                        "properties": {},
+                        "geometry": {
+                          "type": "Point",
+                          "coordinates": [
+                            longitude,
+                            latitude
+                          ]
+                        },
+                        "properties": {
+                          "title": "The Pool ",
+                          "marker-color": "#3BCF56",
+                          "marker-size": "medium",
+                          "marker-symbol": "rocket"
+                        }
+                      }
+                    ]
                   }
-                ]
-              }
-              ];
-var markerLayer = L.mapbox.featureLayer().addTo(map);//we add another layer for the markers and add to the map
-markerLayer.setGeoJSON(marker);//we add the marker to the map
-console.log("si imprime el marcador");
-return marker;
-});
-map.featureLayer.on('ready', function(e) {
-  var line = [];
-  console.log("si entra a la funcion")
-  this.eachLayer(function(marker) {
-    alert(marker.getLatLng());
-    line.push(marker.getLatLng());
-  });
+                  ];
+    var markerLayer = L.mapbox.featureLayer().addTo(map);//we add another layer for the markers and add to the map
 
-  var polyline_options = {
-    color: '#000'
-  };
-
-  var polyline = L.polyline(line, polyline_options).addTo(map);
+    markerLayer.setGeoJSON(marker);//we add the marker to the map
+    document.coordinates.latitude.value = (latitude);//we add the latitude to the input latitude (in the HTML)
+    document.coordinates.longitude.value = (longitude);//we add the longitude to the input longitude(in the HTML)
+    return marker;
 });
 
